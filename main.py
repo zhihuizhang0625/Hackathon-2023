66c0ed82b3db852845e79f9a6ebe4567e16eab67
jocelyn <lluo@brynmawr.edu>
2023-01-16 00:33:56 -0500

    change in main.py

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import requests
 
-AIRTABLE_BASE_ID = ''
-AIRTABLE_API_KEY= 
-AIRTABLE_TABLE_NAME =
+AIRTABLE_BASE_ID = 'appJQ2e3Bms1nlD3B'
+AIRTABLE_API_KEY= 'YOUR_API_KEY'
+AIRTABLE_TABLE_NAME ='leetcode_analysis'
 
-endpoint = f''
+endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
 
 #python requests headers
 def add_to_airtable(email = None, name = ""):
     if email is None:
         return 
-        headers = {"Authorization": f"Bearer {AIRTABLE_API_KEY}",
+        headers = {"Authorization": "Bearer {AIRTABLE_API_KEY}",
         "Content-Type": "application/json"
         }
 
-        data = {
-
-        }
+        data =  {
+          "records": [
+            {
+              "fields": {
+                "Name": name,
+                "Email": email
+              }
+            }
+     
+          ]
+        }       
 
         r= requests.post(endpoint, json = data, headers = headers)
         print(r.status_code)
+
+
+add_to_airtable("abc", "abc@gmail.com")
