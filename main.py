b568d99f52589ebaafb4dc47e3f9cc16e08c2b20
jocelyn <lluo@brynmawr.edu>
2023-01-17 17:50:36 -0500

    create table 2

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ AIRTABLE_API_KEY= 'keyQ0V1l9vBkHkLev'
 #AIRTABLE_TABLE_NAME =''
 AIRTABLE_OAUTH = 'patCSzdYGtv19W5R4.0958fccfc23a810a713ba93534c6c45f4dfe9f4cbeadd45dfbb65b290a73652f'
 
+# table1 -create add update
 # create table
-def create_table(AIRTABLE_TABLE_NAME = ''):
+def create_table1(AIRTABLE_TABLE_NAME = ''):
     endpoint = f'https://api.airtable.com/v0/meta/bases/{AIRTABLE_BASE_ID}/tables'
     headers = {
         "Authorization": "Bearer " + str(AIRTABLE_OAUTH),
@@ -17,7 +18,7 @@ def create_table(AIRTABLE_TABLE_NAME = ''):
         }
 
     data =  {
-    "description": "Testing create_table2",
+    "description": "create_table1",
     "fields": [
       {
         "description": "Problem name",
@@ -26,16 +27,26 @@ def create_table(AIRTABLE_TABLE_NAME = ''):
       },
       {
         "name": "link",
-        "type": "singleLineText"
+        "type": "singleLineText",
       },
       {
         "name": "difficulty",
-        "type": "singleLineText"
+        "type": "singleLineText",
       },
       {
         "name": "status",
-        "type": "singleLineText"
+        "type": "singleLineText",
+      },
+      {
+        "name": "submission",
+        "type": "singleLineText",
+      },
+      {
+        "name": "submission_times",
+        "type": "singleLineText",
       }
+   
+    
     ],
     "name": AIRTABLE_TABLE_NAME
   }       
@@ -44,7 +55,7 @@ def create_table(AIRTABLE_TABLE_NAME = ''):
     print(r)
     
 # import records
-def add_to_airtable(title = "", link = None, difficulty = None, status = None, AIRTABLE_TABLE_NAME = ''):
+def add_to_airtable1(title = "", link = None, difficulty = None, status = None, submission_history = None, submission_times = None, AIRTABLE_TABLE_NAME = ''):
     endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
     headers = {
         "Authorization": "Bearer " + str(AIRTABLE_API_KEY),
@@ -59,6 +70,8 @@ def add_to_airtable(title = "", link = None, difficulty = None, status = None, A
                 "link": link,
                 "difficulty": difficulty,
                 "status": status,
+                "submission": submission_history,
+                "submission_times": submission_times,
               }
             }
      
@@ -69,7 +82,7 @@ def add_to_airtable(title = "", link = None, difficulty = None, status = None, A
     print(r)
 
 #  update records
-def update_records(AIRTABLE_TABLE_NAME = ''):
+def update_records1(AIRTABLE_TABLE_NAME = ''):
     endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
     headers = {
         "Authorization": "Bearer " + str(AIRTABLE_API_KEY),
@@ -101,8 +114,141 @@ def update_records(AIRTABLE_TABLE_NAME = ''):
     print(r)
     
 
+
+#table 2
+# table1 -create add update
+# create table
+def create_table2(AIRTABLE_TABLE_NAME = ''):
+    endpoint = f'https://api.airtable.com/v0/meta/bases/{AIRTABLE_BASE_ID}/tables'
+    headers = {
+        "Authorization": "Bearer " + str(AIRTABLE_OAUTH),
+        "Content-Type": "application/json"
+        }
+
+    data =  {
+    "description": "create_table2",
+    "fields": [
+      {
+        "description": "Submission history",
+        "name": "last_submission_status",
+        "type": "singleLineText",
+      },
+      {
+        "name": "last_submission_time",
+        "type": "singleLineText",
+      },
+      {
+        "name": "last_2th_submission_status",
+        "type": "singleLineText",
+      },
+      {
+        "name": "last_2th_submission_time",
+        "type": "singleLineText",
+      },
+      {
+        "name": "last_3th_submission_status",
+        "type": "singleLineText",
+      },
+      {
+        "name": "last_3th_submission_time",
+        "type": "singleLineText",
+      },
+      {
+        "name": "last_4th_submission_status",
+        "type": "singleLineText",
+      },{
+        "name": "last_4th_submission_time",
+        "type": "singleLineText",
+      },{
+        "name": "last_5th_submission_status",
+        "type": "singleLineText",
+      },{
+        "name": "last_5th_submission_time",
+        "type": "singleLineText",
+      },
+   
+    
+    ],
+    "name": AIRTABLE_TABLE_NAME
+  }       
+
+    r= requests.post(endpoint, json = data, headers = headers)
+    print(r)
+    
+# import records
+def add_to_airtable2(sub1_status = None, sub1_time = None, sub2_status = None, sub2_time = None,sub3_status = None, sub3_time = None,sub4_status = None, sub4_time = None,sub5_status = None, sub5_time = None, AIRTABLE_TABLE_NAME = ''):
+    endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
+    headers = {
+        "Authorization": "Bearer " + str(AIRTABLE_API_KEY),
+        "Content-Type": "application/json"
+        }
+
+    data =  {
+          "records": [
+            {
+              "fields": {
+                "last_submission_status": sub1_status,
+                "last_submission_time": sub1_time,
+                "last_2th_submission_status": sub2_status,
+                "last_2th_submission_time": sub2_time,
+                "last_3th_submission_status": sub3_status,
+                "last_3th_submission_time": sub3_time,
+                "last_4th_submission_status": sub4_status,
+                "last_4th_submission_time": sub4_time,
+                "last_5th_submission_status": sub5_status,
+                "last_5th_submission_time": sub5_time,
+                
+                
+                
+                
+              }
+            }
+     
+          ]
+        }       
+
+    r= requests.post(endpoint, json = data, headers = headers)
+    print(r)
+
+#  update records
+def update_records2(sub1_status = None, sub1_time = None, sub2_status = None, sub2_time = None,sub3_status = None, sub3_time = None,sub4_status = None, sub4_time = None,sub5_status = None, sub5_time = None,AIRTABLE_TABLE_NAME = ''):
+    endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
+    headers = {
+        "Authorization": "Bearer " + str(AIRTABLE_API_KEY),
+        "Content-Type": "application/json"
+        }
+
+    data = {
+    "records": [
+      {
+         "fields": {
+                "last_submission_status": sub1_status,
+                "last_submission_time": sub1_time,
+                "last_2th_submission_status": sub2_status,
+                "last_2th_submission_time": sub2_time,
+                "last_3th_submission_status": sub3_status,
+                "last_3th_submission_time": sub3_time,
+                "last_4th_submission_status": sub4_status,
+                "last_4th_submission_time": sub4_time,
+                "last_5th_submission_status": sub5_status,
+                "last_5th_submission_time": sub5_time,
+                
+        },
+        "id": "recqhmz9ZSZFSh8WP"
+      }
+    ]
+  } 
+
+    r= requests.patch(endpoint, json = data, headers = headers)
+    print(endpoint)
+    
+    print(r)
+    
+
+
 def load_sample():
-    create_table('test')
+    create_table1('summary')
+    create_table2('submission_history')
 
     # Opening JSON file
     f = open('sample.json')
@@ -115,8 +261,38 @@ def load_sample():
     # Iterating through the json
     # list
     for i in data:
-      add_to_airtable(i['title'], i['link'],i['difficulty'],i['status'], 'test')
-      print(i)
+      add_to_airtable1(i['title'], i['link'],i['difficulty'],i['status'],str(i['submission_history']),i['submission_times'], 'summary')
+      
+      if(len(i['submission_history']) < 5):
+
+        sub_status = []
+        sub_time = []
+        num = len(i['submission_history'])
+
+        for j in range(num):
+          #print(j)
+          sub_status.append(i['submission_history'][-(j+1)]['submission_status'])
+          print(sub_status[j])
+          sub_time.append(i['submission_history'][-(j+1)]['submission_time'])
+        
+        for k in range(num , 5) :
+          sub_status.append(None)
+          sub_time.append(None)
+
+        add_to_airtable2(sub_status[0], sub_time[0], sub_status[1], sub_time[1], sub_status[2], sub_time[2], sub_status[3], sub_time[3], sub_status[4], sub_time[4], 'submission_history')
+
+      else: add_to_airtable2(i['submission_history'][-1]['submission_status'],\
+                          i['submission_history'][-1]['submission_time'],\
+                          i['submission_history'][-2]['submission_status'],\
+                          i['submission_history'][-2]['submission_time'],\
+                          i['submission_history'][-3]['submission_status'],\
+                          i['submission_history'][-3]['submission_time'],\
+                          i['submission_history'][-4]['submission_status'],\
+                          i['submission_history'][-4]['submission_time'],\
+                          i['submission_history'][-5]['submission_status'],\
+                          i['submission_history'][-5]['submission_time'],\
+                          'submission_history')
+
 
     # Closing file
     f.close()
