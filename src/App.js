56120e0e9947cbb9ee8a5559551b6c61ef322375
Clara Zhang <85580892+zhihuizhang0625@users.noreply.github.com>
2023-01-18 15:17:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,38 @@ import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 
+import Airtable from "airtable";
+
+const base = new Airtable({apiKey: 'keyQ0V1l9vBkHkLev'}).base('appJQ2e3Bms1nlD3B');
+
+
+
 function App() {
+  
+  const [summary, setSummary] = useState([])
+  const [submission_history, setSubmission_history] = useState([])
+
+  useEffect(()=>{
+    base("summary")
+      .select({view: "Grid view"})
+      .eachPage((records, fetchNextPage)=>{
+        console.log(records[0].fields);
+        setSummary(records);
+        fetchNextPage();
+      })
+      base("submission_history")
+      .select({view: "Grid view"})
+      .eachPage((records, fetchNextPage)=>{
+        console.log(records[0].fields);
+        setSubmission_history(records);
+        fetchNextPage();
+      })
+   
+
+  }, []);
   const { darkMode } = useContext(DarkModeContext);
 
   return (
@@ -17,22 +45,26 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/">
+
             <Route index element={<Home />} />
+
             <Route path="login" element={<Login />} />
+
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
               <Route
                 path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
+                element={<New inputs={userInputs} title="Add Leetcode Problem" />}
               />
             </Route>
+
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
               <Route
                 path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
+                element={<New inputs={productInputs} title="Add Failed Problem" />}
               />
             </Route>
           </Route>
